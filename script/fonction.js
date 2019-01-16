c772b68fae3c3fcdb53d05c4bd80e36f293f19bd
Philippe Larose <plarose900@gmail.com>
2019-01-16 18:58:45 -0500

    séparation du code en cour

---
+++ script/fonction.js
@@ -0,0 +1,85 @@
+var max_y = screen.height * 0.45;
+var max_x = screen.width;
+var m_y = max_y;
+var m_x = 1;
+var m_x_monstre1 = screen.width * 0.5;
+var m_lock = 0;
+var m_nbSaut = 0;
+var m_direction = 0;
+var gravite;
+var m_niveau = 1;
+var m_hauteurSaut = 30;
+var m_vitesseMonstre = 2;
+setInterval(methodeGravite, 20);
+setInterval(methodeLevel, 30);
+function zxcKeyPress(zxc) {
+  var touche = String.fromCharCode(zxc);
+
+  if (touche === "W") {
+    if (m_lock === 0) {
+      m_lock = 1;
+      m_y = m_y - 60;
+    }
+
+    var d = document.getElementById("personne1");
+    d.style.position = "absolute";
+    d.style.left = m_x + "px";
+    d.style.top = m_y + "px";
+  }
+  if (touche === "D") {
+    m_direction = 2;
+    m_x = m_x + 10;
+    var d = document.getElementById("personne1");
+
+    d.src = "shrek/shrek_mini_rightv2.png";
+    d.style.position = "absolute";
+    d.style.left = m_x + "px";
+    d.style.top = m_y + "px";
+  }
+
+  if (touche === "A") {
+    m_direction = -2;
+    m_x = m_x - 10;
+    var d = document.getElementById("personne1");
+
+    d.src = "shrek/shrek_mini_left.png";
+    d.style.position = "absolute";
+    d.style.left = m_x + "px";
+    d.style.top = m_y + "px";
+  }
+}
+
+function zxcSetDiv(zxc) {
+  zxcObj = zxc;
+  document.onkeydown = function(evt) {
+    zxcKeyPress(evt ? evt.keyCode : event.keyCode);
+  };
+
+  zxc.onmouseout = function() {
+    document.onkeydown = null;
+  };
+}
+function methodeInitialize() {
+  m_y = max_y;
+  if (m_niveau === 5) {
+    m_x_monstre1 = screen.width * 2;
+  } else {
+    m_x_monstre1 = screen.width * 0.5;
+  }
+  m_x = 1;
+  var d = document.getElementById("personne1");
+  d.style.position = "absolute";
+
+  d.src = "shrek/shrek_mini_happy.png";
+  d.style.left = m_x + "px";
+  d.style.top = m_y + "px";
+}
+function hitBox(source, target) {
+  /* Box model detection, return true on collision */
+  return !(
+    source.offsetTop + source.offsetHeight < target.offsetTop ||
+    source.offsetTop > target.offsetTop + target.offsetHeight ||
+    source.offsetLeft + source.offsetWidth < target.offsetLeft ||
+    source.offsetLeft > target.offsetLeft + target.offsetWidth
+  );
+}
